ef4494c17e2938df4ae19d70a9b1ff2c0962fa66
John Rossi <DACOOK1983@gamil.com>
2020-02-20 15:02:36 -0500

    thought mvp was done but is now, and stretch goal complete

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ const siteContent = {
     "nav-item-4": "Features",
     "nav-item-5": "About",
     "nav-item-6": "Contact",
-    "img-src": "img/logo.png"
+    "img-src": ["img/logo.png", "img/logo1.png"]
   },
   "cta": {
     "h1": "DOM \n Is \n Awesome",
@@ -36,10 +36,20 @@ const siteContent = {
     "copyright" : "Copyright Great Idea! 2018"
   },
 };
-
+console.log(siteContent["nav"]["img-src"].length)
 // Example: Update the img src for the logo
+  
+
+
 let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+  var i = 1;
+  // while (i < 1){
+  //   i++
+  // } 
+  
+
+logo.setAttribute('src', siteContent["nav"]["img-src"][i])
+
 // nav bar
 const navList = document.querySelectorAll('a');
 navList[0].textContent = siteContent['nav']['nav-item-1'];
@@ -48,6 +58,28 @@ navList[2].textContent = siteContent['nav']['nav-item-3'];
 navList[3].textContent = siteContent['nav']['nav-item-4'];
 navList[4].textContent = siteContent['nav']['nav-item-5'];
 navList[5].textContent = siteContent['nav']['nav-item-6'];
+
+navList.forEach((element, index) => element.style.color = "green");
+
+// new nav items
+let nav = document.querySelector("nav");
+
+let newNav1 = document.createElement("a");
+let newNav2 = document.createElement("a");
+let n1 = document.createTextNode("Twitter");
+let n2 = document.createTextNode("Facebook");
+
+newNav1.appendChild(n1);
+newNav2.appendChild(n2);
+
+nav.appendChild(newNav1);
+nav.prepend(newNav2);
+
+newNav1.style.color = "green";
+newNav2.style.color = "green";
+
+newNav1.setAttribute("href", "#");
+newNav2.setAttribute("href", "#");
 console.log(navList);
 
 // cta section
@@ -58,10 +90,11 @@ console.log(navList);
 
 let ctaText = document.querySelector('h1');
 ctaText.innerText = siteContent['cta']['h1'];
+ctaText.style.color = "green";
 
 let btnText = document.querySelector('button');
 btnText.innerText = siteContent['cta']['button'];
-
+btnText.style.color = "green";
 //main content
 
 const contentHeaders = document.querySelectorAll('h4');
@@ -72,23 +105,29 @@ contentImg.setAttribute('src', siteContent ["main-content"]["middle-img-src"]);
 console.log(contentHeaders);
 
 contentHeaders[0].textContent = siteContent['main-content']['features-h4'];
+contentHeaders[0].style.color = "green";
 contentPara[0].innerHTML = siteContent['main-content']['features-content'];
 
 contentHeaders[1].textContent = siteContent['main-content']['about-h4'];
+contentHeaders[1].style.color = "green";
 contentPara[1].innerHTML = siteContent['main-content']['about-content'];
 
 contentHeaders[2].textContent = siteContent['main-content']['services-h4'];
+contentHeaders[2].style.color = "green";
 contentPara[2].innerHTML = siteContent['main-content']['services-content'];
 
 contentHeaders[3].textContent = siteContent['main-content']['product-h4'];
+contentHeaders[3].style.color = "green";
 contentPara[3].innerHTML = siteContent['main-content']['product-content'];
 
 contentHeaders[4].textContent = siteContent['main-content']['vision-h4'];
+contentHeaders[4].style.color = "green";
 contentPara[4].innerHTML = siteContent['main-content']['vision-content'];
 
 //contact
 
 contentHeaders[5].textContent = siteContent['contact']['contact-h4'];
+contentHeaders[5].style.color = "green";
 contentPara[5].innerText = siteContent['contact']['address'];
 contentPara[6].textContent = siteContent['contact']['phone'];
 contentPara[7].innerText = siteContent['contact']['email'];
